cd3c5a1a6b281187b838b7a9cc8906d5b6b3fae6
DucThacc <ducthanh300904@gmail.com>
2025-10-08 22:14:18 +0700

    SQLi fix login

--- Website_test_SEC/model/taikhoan.php
+++ Website_test_SEC/model/taikhoan.php
@@ -1,67 +1,71 @@
 <?php 
 
 function insert_taikhoan($tenuser,$accountuser,$passuser,$mailuser,$teluser,$addressuser){
-    $sql="INSERT INTO tbl_user(tenUser,accountUser,passUser,emailUser,addressUser,telUser,decen) values ('$tenuser','$accountuser','$passuser','$mailuser','$addressuser','$teluser',0) ";
-    pdo_execute($sql);
+    $sql="INSERT INTO tbl_user(tenUser,accountUser,passUser,emailUser,addressUser,telUser,decen) values (?,?,?,?,?, ?,0) ";
+    pdo_execute($sql, $tenuser, $accountuser, $passuser, $mailuser, $addressuser, $teluser);
 }
 
 function insert_taikhoan_nguoidung($accountuser,$passuser){
-    $sql="INSERT INTO tbl_user(tenUser,accountUser,passUser,emailUser,addressUser,telUser,decen) values ('$accountuser','$accountuser','$passuser','', '', '', 0) ";
-    pdo_execute($sql);
+    $sql="INSERT INTO tbl_user(tenUser,accountUser,passUser,emailUser,addressUser,telUser,decen) values (?,?,?, '', '', '', 0) ";
+    pdo_execute($sql, $accountuser, $accountuser, $passuser);
 }
 
 
 function checkuser($user,$pass){
-    $sql="SELECT * FROM tbl_user WHERE accountUser='".$user."' AND passUser='".$pass."'";
-    $sp=pdo_query_one($sql);
+    $sql="SELECT * FROM tbl_user WHERE accountUser = ? AND passUser = ?";
+    $sp=pdo_query_one($sql, $user, $pass);
     return $sp;
 }
 
 function deleted_user($id){
-    $sql="DELETE FROM tbl_user WHERE idUser=".$id;
-    pdo_execute($sql);
+    $sql="DELETE FROM tbl_user WHERE idUser = ?";
+    pdo_execute($sql, $id);
 }
 
 function selectUser($iduser){
-    $sql="SELECT * FROM tbl_user WHERE idUser='".$iduser."'";
-    $sp=pdo_query_one($sql);
+    $sql="SELECT * FROM tbl_user WHERE idUser = ?";
+    $sp=pdo_query_one($sql, $iduser);
     return $sp;
 }
 
 function checkemail($email){
-    $sql="SELECT * FROM tbl_user WHERE email='".$email."'";
-    $sp=pdo_query_one($sql);
+    $sql="SELECT * FROM tbl_user WHERE email = ?";
+    $sp=pdo_query_one($sql, $email);
     return $sp;
 }
 
 function update_taikhoan($iduser,$tenuser,$emailuser,$teluser,$addressuser){
-    $sql="UPDATE tbl_user SET tenUser='".$tenuser."',emailUser='".$emailuser."',telUser='".$teluser."',addressUser='".$addressuser."'  WHERE idUser=".$iduser;
-    pdo_execute($sql);
+    $sql="UPDATE tbl_user SET tenUser = ?, emailUser = ?, telUser = ?, addressUser = ? WHERE idUser = ?";
+    pdo_execute($sql, $tenuser, $emailuser, $teluser, $addressuser, $iduser);
 }
 
 function update_taikhoan_admin($iduser,$tenuser,$accountuser,$passuser,$emailuser,$teluser,$addressuser){
-    $sql="UPDATE tbl_user SET tenUser='".$tenuser."',emailUser='".$emailuser."',telUser='".$teluser."',addressUser='".$addressuser."',accountUser='".$accountuser."',passUser='".$passuser."'  WHERE idUser=".$iduser;
-    pdo_execute($sql);
+    $sql="UPDATE tbl_user SET tenUser = ?, emailUser = ?, telUser = ?, addressUser = ?, accountUser = ?, passUser = ? WHERE idUser = ?";
+    pdo_execute($sql, $tenuser, $emailuser, $teluser, $addressuser, $accountuser, $passuser, $iduser);
 }
 
 function update_pass($new_pass,$iduser){
-    $sql="UPDATE tbl_user SET  passUser='".$new_pass."' WHERE idUser=".$iduser;
-    pdo_execute($sql);
+    $sql="UPDATE tbl_user SET  passUser = ? WHERE idUser = ?";
+    pdo_execute($sql, $new_pass, $iduser);
 }
 
 function account_exists($accountuser){
-    $sql="SELECT 1 FROM tbl_user WHERE accountUser='".$accountuser."' LIMIT 1";
-    $sp=pdo_query_one($sql);
+    $sql="SELECT 1 FROM tbl_user WHERE accountUser = ? LIMIT 1";
+    $sp=pdo_query_one($sql, $accountuser);
     return $sp ? true : false;
 }
 
 function loadall_taikhoan($kyw){
     $sql="SELECT * FROM tbl_user WHERE 1";
     if($kyw!=""){
-        $sql.=" and tenUser like '%".$kyw."%'";
+        $sql.=" and tenUser like ?";
     }
     $sql.=" ORDER BY idUser DESC";
-    $listtaikhoan=pdo_query($sql);
+    if($kyw!=""){
+        $listtaikhoan=pdo_query($sql, "%".$kyw."%");
+    } else {
+        $listtaikhoan=pdo_query($sql);
+    }
     return $listtaikhoan;
 }
 
